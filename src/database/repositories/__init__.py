389d74b8335fb3ab7a7f3737abd01e2d1664c1d9
Jesús Lorenzo Limón <jlorenzolimon@gmail.com>
2026-01-04 23:48:15 +0100

    fix lint

--- src/database/repositories/__init__.py
+++ src/database/repositories/__init__.py
@@ -1,10 +1,10 @@
 # Repositories package
 from src.database.repositories.competition import CompetitionRepository
-from src.database.repositories.event import EventRepository
-from src.database.repositories.user import UserRepository
-from src.database.repositories.subscription import SubscriptionRepository
-from src.database.repositories.notification import NotificationRepository
 from src.database.repositories.error import ErrorRepository
+from src.database.repositories.event import EventRepository
+from src.database.repositories.notification import NotificationRepository
+from src.database.repositories.subscription import SubscriptionRepository
+from src.database.repositories.user import UserRepository
 
 __all__ = [
     "CompetitionRepository",
